362315cebe224f0cff7c7d83c9176a0bd1fb3de8
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-02 12:05:57 +0600

    Change

--- White.Lagoon.Domain/Entities/Villa.cs
+++ White.Lagoon.Domain/Entities/Villa.cs
@@ -8,6 +8,14 @@ namespace White.Lagoon.Domain.Entities
 {
     public class Villa
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double price { get; set; }
+        public int Sqft { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get;set; }
 
     }
 }
