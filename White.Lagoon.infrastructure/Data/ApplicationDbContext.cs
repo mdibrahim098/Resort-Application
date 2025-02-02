3641b830416fea11262dfedd9b8ea6cf00add474
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-02 16:44:44 +0600

    change

---
+++ White.Lagoon.infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace White.Lagoon.infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
