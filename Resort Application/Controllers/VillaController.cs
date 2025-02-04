4a2ee7f019712eb0d63898cfd0714ccf30cac9da
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-04 14:27:03 +0600

    Change

---
+++ Resort Application/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using White.Lagoon.infrastructure.Data;
+
+namespace Resort_Application.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View();
+        }
+    }
+}
