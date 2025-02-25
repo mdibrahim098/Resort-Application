a8942665a7d1f2fe941b7c672abbbaeee9426801
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-25 17:00:45 +0600

    Change 88

--- White.Lagoon.infrastructure/Repository/Repository.cs
+++ White.Lagoon.infrastructure/Repository/Repository.cs
@@ -4,30 +4,70 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.Domain.Entities;
+using White.Lagoon.infrastructure.Data;
 
 namespace White.Lagoon.infrastructure.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
+
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);  
         }
 
         public T Get(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                // Villa, VillaNumber -- Case sensitive
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefault();
         }
 
         public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+               
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.ToList();
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
         }
     }
 }
--- White.Lagoon.infrastructure/Repository/VillaRepository.cs
+++ White.Lagoon.infrastructure/Repository/VillaRepository.cs
@@ -11,63 +11,16 @@ using White.Lagoon.infrastructure.Data;
 
 namespace White.Lagoon.infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository :Repository<Villa>, IVillaRepository
     {
 
         private readonly ApplicationDbContext _db;
 
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
-        public void Add(Villa entity)
-        {
-            _db.Add(entity);
-        }
-
-        public Villa Get(Expression<Func<Villa, bool>>? filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                // Villa, VillaNumber -- Case sensitive
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(includeProp);
-                }
-            }
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-           IQueryable<Villa> query = _db.Set<Villa>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                // Villa, VillaNumber -- Case sensitive
-                foreach(var includeProp in includeProperties
-                    .Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
-                {
-                   query = query.Include(includeProp);
-                }
-            }
-            return query.ToList();
-        }
-
-        public void Remove(Villa entity)
-        {
-            _db.Remove(entity);
-        }
-
+     
         public void Save()
         {
             _db.SaveChanges();
