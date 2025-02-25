a850c8f0d5398b0e8d92d20160df47086fc9a032
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-25 19:02:53 +0600

    Change 94

--- White.Lagoon.Domain/Entities/Villa.cs
+++ White.Lagoon.Domain/Entities/Villa.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace White.Lagoon.Domain.Entities
 {
@@ -19,6 +21,8 @@ namespace White.Lagoon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1, 10)]
         public double Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
