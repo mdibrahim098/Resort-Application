a939482c9b62b7017b5b76d013a6b5aea306eea6
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-25 17:27:23 +0600

    Change 89

---
+++ White.Lagoon.infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.infrastructure.Data;
+
+namespace White.Lagoon.infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IVillaRepository Villa { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Villa = new VillaRepository(_db);
+        }
+    }
+}
