a8b359dc5c8e553f1ed95f2561706d794c747eae
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-25 16:18:17 +0600

    Change 85

--- White.Lagoon.infrastructure/Repository/VillaRepository.cs
+++ White.Lagoon.infrastructure/Repository/VillaRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using White.Lagoon.Application.Common.Interfaces;
 using White.Lagoon.Domain.Entities;
 using White.Lagoon.infrastructure.Data;
@@ -24,14 +25,42 @@ namespace White.Lagoon.infrastructure.Repository
             _db.Add(entity);
         }
 
-        public IEnumerable<Villa> Get(Expression<Func<Villa, bool>>? filter, string? includeProperties = null)
+        public Villa Get(Expression<Func<Villa, bool>>? filter, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<Villa> query = _db.Set<Villa>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                // Villa, VillaNumber -- Case sensitive
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefault();
         }
 
         public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+           IQueryable<Villa> query = _db.Set<Villa>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                // Villa, VillaNumber -- Case sensitive
+                foreach(var includeProp in includeProperties
+                    .Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                   query = query.Include(includeProp);
+                }
+            }
+            return query.ToList();
         }
 
         public void Remove(Villa entity)
