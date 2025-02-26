c48478ee83554384fc4c05a40b59d0f926d6c60a
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-26 16:54:02 +0600

    Change 124

---
+++ Resort Application/Controllers/AccountController1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.Domain.Entities;
+
+namespace Resort_Application.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public  AccountController1(IUnitOfWork unitOfWork, 
+                UserManager<ApplicationUser> userManager, 
+                SignInManager<ApplicationUser> signInManager, 
+                RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
