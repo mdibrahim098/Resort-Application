72f7210544e9ff35e3cc9333452fcc670701e43e
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-26 13:41:03 +0600

    Change 110

--- Resort Application/Controllers/HomeController.cs
+++ Resort Application/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Resort_Application.Models;
+using Resort_Application.ViewModels;
+using White.Lagoon.Application.Common.Interfaces;
 
 namespace Resort_Application.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now),
+
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
