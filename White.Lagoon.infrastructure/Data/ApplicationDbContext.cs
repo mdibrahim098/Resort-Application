3957e00b510e33b894c13ba85829a564e790c222
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-26 12:00:45 +0600

    Change 102

--- White.Lagoon.infrastructure/Data/ApplicationDbContext.cs
+++ White.Lagoon.infrastructure/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace White.Lagoon.infrastructure.Data
         public DbSet<Villa> Villas { get; set; }
         public DbSet<VillaNumber> VillaNumbers { get; set; }
 
+        public DbSet<Amenity> Amenities { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //base.OnModelCreating(modelBuilder);
@@ -31,7 +32,7 @@ namespace White.Lagoon.infrastructure.Data
                       Price = 200,
                       Sqft = 550,
                                       },
-                    new Villa
+                  new Villa
                     {
                         Id = 2,
                         Name = "Premium Pool Villa",
@@ -41,7 +42,7 @@ namespace White.Lagoon.infrastructure.Data
                         Price = 300,
                         Sqft = 550,
                     },
-                    new Villa
+                  new Villa
                     {
                         Id = 3,
                         Name = "Luxury Pool Villa",
@@ -51,9 +52,7 @@ namespace White.Lagoon.infrastructure.Data
                         Price = 400,
                         Sqft = 750,
                     }
-
                 );
-
 
             modelBuilder.Entity<VillaNumber>().HasData(
                 new VillaNumber
@@ -62,56 +61,117 @@ namespace White.Lagoon.infrastructure.Data
                     VillaId = 1
                   
                 },
-                 new VillaNumber
+                new VillaNumber
                  {
                      Villa_number = 102,
                      VillaId = 1
 
                  },
-                  new VillaNumber
+                new VillaNumber
                   {
                       Villa_number = 103,
                       VillaId = 1
 
                   },
-                   new VillaNumber
+                new VillaNumber
                    {
                        Villa_number = 104,
                        VillaId = 1
 
                    },
-                    new VillaNumber
+                new VillaNumber
                     {
                         Villa_number = 201,
                         VillaId = 2
 
                     },
-                      new VillaNumber
+                new VillaNumber
                       {
                           Villa_number = 202,
                           VillaId = 2
 
                       },
-                        new VillaNumber
+                new VillaNumber
                         {
                             Villa_number = 203,
                             VillaId = 2
 
                         },
-                          new VillaNumber
+                new VillaNumber
                           {
                               Villa_number = 301,
                               VillaId = 3
 
                           },
-                            new VillaNumber
+                new VillaNumber
                             {
                                 Villa_number = 302,
                                 VillaId = 3
 
                             }
-                            );
+              );
 
+            modelBuilder.Entity<Amenity>().HasData(
+          new Amenity
+          {
+              Id = 1,
+              VillaId = 1,
+              Name = "Private Pool"
+          }, new Amenity
+          {
+              Id = 2,
+              VillaId = 1,
+              Name = "Microwave"
+          }, new Amenity
+          {
+              Id = 3,
+              VillaId = 1,
+              Name = "Private Balcony"
+          }, new Amenity
+          {
+              Id = 4,
+              VillaId = 1,
+              Name = "1 king bed and 1 sofa bed"
+          },
+
+          new Amenity
+          {
+              Id = 5,
+              VillaId = 2,
+              Name = "Private Plunge Pool"
+          }, new Amenity
+          {
+              Id = 6,
+              VillaId = 2,
+              Name = "Microwave and Mini Refrigerator"
+          }, new Amenity
+          {
+              Id = 7,
+              VillaId = 2,
+              Name = "Private Balcony"
+          }, new Amenity
+          {
+              Id = 8,
+              VillaId = 2,
+              Name = "king bed or 2 double beds"
+          },
+
+          new Amenity
+          {
+              Id = 9,
+              VillaId = 3,
+              Name = "Private Pool"
+          }, new Amenity
+          {
+              Id = 10,
+              VillaId = 3,
+              Name = "Jacuzzi"
+          }, new Amenity
+          {
+              Id = 11,
+              VillaId = 3,
+              Name = "Private Balcony"
+          });
 
         }
     }
