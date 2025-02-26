c5c4b406fed444638763baef5e8ee520405297a4
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-26 19:25:14 +0600

    Change 127

--- Resort Application/Controllers/AccountController.cs
+++ Resort Application/Controllers/AccountController.cs
@@ -35,6 +35,11 @@ namespace Resort_Application.Controllers
         }
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
             return View();
         }
 
