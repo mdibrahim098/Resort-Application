757be39bd413a90b8ad7f3506226bd870cf83487
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-02-27 11:34:44 +0600

    Change 130

--- Resort Application/Controllers/AccountController.cs
+++ Resort Application/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Resort_Application.ViewModels;
@@ -51,6 +52,57 @@ namespace Resort_Application.Controllers
                     Value = x.Name
                 })
             };
+
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            ApplicationUser user = new()
+            {
+                Name = registerVM.Name,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+                NormalizedEmail = registerVM.Email.ToUpper(),
+                EmailConfirmed = true,
+                UserName = registerVM.Email,
+                CreateAt = DateTime.Now 
+            };
+
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+           
+            if(result.Succeeded)
+            {
+                if(!string.IsNullOrEmpty(registerVM.Role))
+                {
+                    await _userManager.AddToRoleAsync(user, registerVM.Role);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                }
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                if(!string.IsNullOrEmpty(registerVM.RedirectUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return LocalRedirect(registerVM.RedirectUrl);
+                }
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            registerVM.RoleList = _roleManager.Roles.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Name
+            });
 
             return View(registerVM);
         }
