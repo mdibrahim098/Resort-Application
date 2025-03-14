e3c50ae4e42f6c62df8cab1609adff6b167097ab
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-15 04:06:51 +0600

    Change 205

---
+++ Resort Application/ViewModels/PieChartVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Resort_Application.ViewModels
+{
+    public class PieChartVM
+    {
+        public decimal[] Seriecs { get; set; }
+        public string[] Labels { get; set; }
+
+    }
+}
