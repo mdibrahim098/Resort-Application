dabbd3d27173a29ea3ea6d7ef56c2684b0d4c2f2
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-15 01:06:42 +0600

    Change 199

--- Resort Application/wwwroot/js/dashboard/getTotalBookingsRadial.js
+++ Resort Application/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -38,31 +38,3 @@ function loadTotalBokingRadialChart() {
 
 
 
-function loadRadialBarChart(id,data)
-{
-    var options = {
-        chart: {
-            height: 90,
-            width : 90,
-            type: "radialBar",
-            sparkline: {
-                enable: true,
-            },
-            offsetY : -10,
-        },
-        series: data.series,
-        plotOptions: {
-            radialBar: {
-                dataLabels: {
-                    value: {
-                        offsetY: -10,
-                    }
-                }
-            }
-        },
-        labels: [""]
-    };
-    var chart = new ApexCharts(document.querySelector("#" + id), options);
-    chart.render();
-
-}
