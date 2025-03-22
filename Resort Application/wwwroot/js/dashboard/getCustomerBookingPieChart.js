7e2b19eed199521085b6e06bf7035c8e72fb0644
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-23 00:22:54 +0600

    Change 208

---
+++ Resort Application/wwwroot/js/dashboard/getCustomerBookingPieChart.js
@@ -0,0 +1,51 @@
+﻿
+
+$(document).ready(function () {
+    getCustomerBookingPieChart();
+
+});
+
+
+function getCustomerBookingPieChart() {
+    $(".chart-spiner").show();
+
+    $.ajax({
+        url: '/Dashboard/GetBookingPieChartData',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            loadPieChart("customerBookingsPieChart", data);
+
+            $(".chart-spiner").hide();
+        }
+    });
+};
+
+
+function loadPieChart(id,data) {
+    var chartColors = getChartColorsArray(id);
+    var options = {
+        colors: chartColors,
+        series: data.series,
+        labels: data.labels,
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        stroke: {
+            show: false
+        },
+        legend: {
+            position: 'bottom',
+            horizontalAlign: "center",
+            labels: {
+                colors: "#fff",
+                useSeriesColors: true
+            },
+        },
+    };
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+
+
+};
