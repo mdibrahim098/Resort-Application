d73306ba33c1b4b73675b11e82e62db2f54eb3e6
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-27 03:12:52 +0600

    Change 231

--- White.Lagoon.infrastructure/Data/DbInitializer.cs
+++ White.Lagoon.infrastructure/Data/DbInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using White.Lagoon.Application.Common.Interfaces;
@@ -39,7 +40,23 @@ namespace White.Lagoon.infrastructure.Data
                 {
                     _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
                     _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
+                   
+                    
+                      _userManager.CreateAsync(new ApplicationUser
+                    {
+                        UserName = "admin@dotnetmastery.com",
+                        Email = "admin@dotnetmastery.com",
+                        Name = "Ibrahim",
+                        NormalizedUserName = "ADMIN@DOTNETMASTERY.COM",
+                        NormalizedEmail = "ADMIN@DOTNETMASTERY.COM",
+                        PhoneNumber = "1112223333",
+                    }, "Admin123*").GetAwaiter().GetResult();
+
+                    ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetmastery.com");
+                    _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
                 }
+              
 
             }
             catch (Exception e)
