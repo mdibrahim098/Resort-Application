14d31b10f10d99874da0e38071e368327ddfe249
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-31 03:24:45 +0600

    Change 244

--- Resort Application/Program.cs
+++ Resort Application/Program.cs
@@ -41,6 +41,7 @@ builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddScoped<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 builder.Services.AddScoped<IAmenityService, AmenityService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 
 
---
+++ White.Lagoon.Application/Services/Implementation/BookingService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.Application.Services.Interface;
+using White.Lagoon.Domain.Entities;
+
+namespace White.Lagoon.Application.Services.Implementation
+{
+    public class BookingService : IBookingService
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BookingService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateBooking(Booking booking)
+        {
+           _unitOfWork.Booking.Add(booking);
+            _unitOfWork.Save();
+        }
+
+        public IEnumerable<Booking> GetAllBookings(string userId = "", string? statusFilterList = "")
+        {
+            IEnumerable<string> statusList = statusFilterList.ToLower().Split(',');
+            if (!string.IsNullOrEmpty(statusFilterList) && !string.IsNullOrEmpty(userId))
+            {
+                return _unitOfWork.Booking.GetAll(u => statusList.Contains(u.Status.ToLower()) &&
+                        u.UserId == userId, includeProperties: "User,Villa");
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(statusFilterList))
+                {
+                    return _unitOfWork.Booking.GetAll(u => statusList.Contains(u.Status.ToLower())
+                          , includeProperties: "User,Villa");
+                }
+                else if (!string.IsNullOrEmpty(userId))
+                {
+                    return _unitOfWork.Booking.GetAll(u => u.UserId == userId, includeProperties: "User,Villa");
+                }
+            }
+
+            return _unitOfWork.Booking.GetAll(includeProperties: "User,Villa");
+
+        }
+
+        public Booking GetBookingById(int BookingId)
+        {
+            return _unitOfWork.Booking.Get(u => u.Id == BookingId, includeProperties: "User,Villa");
+        }
+    }
+}
