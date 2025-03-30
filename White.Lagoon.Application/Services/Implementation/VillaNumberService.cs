66546e920476115e7adac662d0983e990b6ca9b4
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-30 22:47:37 +0600

    Change 239

---
+++ White.Lagoon.Application/Services/Implementation/VillaNumberService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.Application.Services.Interface;
+using White.Lagoon.Domain.Entities;
+
+namespace White.Lagoon.Application.Services.Implementation
+{
+    public class VillaNumberService : IVillaNumberService
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public VillaNumberService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Add(villaNumber);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteVillaNumber(int id)
+        {
+
+            try
+            {
+                VillaNumber? objFromDb = _unitOfWork.VillaNumber.Get(u => u.Villa_number == id);
+                if (objFromDb is not null)
+                {
+                    _unitOfWork.VillaNumber.Remove(objFromDb);
+                    _unitOfWork.Save();
+                    return true;
+                }
+                return false;
+                  
+           }catch (Exception)
+            {
+                return false;
+            }
+
+
+            
+        }
+
+        public IEnumerable<VillaNumber> GetAllVillaNumbers()
+        {
+            return _unitOfWork.VillaNumber.GetAll();
+        }
+
+        public VillaNumber GetVillaNumberById(int id)
+        {
+            return _unitOfWork.VillaNumber.Get(u => u.Villa_number == id);
+        }
+
+        public void UpdateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Update(villaNumber);
+            _unitOfWork.Save();
+        }
+    }
+}
