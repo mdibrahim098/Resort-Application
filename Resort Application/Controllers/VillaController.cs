d7389e5f7925dbd1f2133fcc4a3d9f43c968c5a6
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-30 22:14:03 +0600

    Change 237

--- Resort Application/Controllers/VillaController.cs
+++ Resort Application/Controllers/VillaController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using White.Lagoon.Application.Common.Interfaces;
+using White.Lagoon.Application.Services.Implementation;
+using White.Lagoon.Application.Services.Interface;
 using White.Lagoon.Domain.Entities;
 using White.Lagoon.infrastructure.Data;
 
@@ -10,17 +12,16 @@ namespace Resort_Application.Controllers
     [Authorize]
     public class VillaController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+       
+        private readonly IVillaService _villaService;
 
-        public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        public VillaController(IVillaService villaService)
         {
-            _unitOfWork = unitOfWork;
-            _webHostEnvironment = webHostEnvironment;
+            _villaService = villaService;
         }
         public IActionResult Index()
         {
-            var villas = _unitOfWork.Villa.GetAll();
+            var villas = _villaService.GetAllVillas();
             return View(villas);
         }
         public IActionResult Create()
@@ -37,24 +38,8 @@ namespace Resort_Application.Controllers
             }
             if (ModelState.IsValid)
             {
-                if (Obj.Image != null)
-                {
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(Obj.Image.FileName);
-                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
-                    using (var fileStream = new FileStream(Path.Combine(imagePath,fileName),FileMode.Create))
-                           Obj.Image.CopyTo(fileStream);
-
-                           Obj.ImageUrl = @"\images\VillaImage\" + fileName;
-
-                }
-                else
-                {
-                    Obj.ImageUrl = "https://placehold.co/600x400";
-                }
-
-                _unitOfWork.Villa.Add(Obj);
-                _unitOfWork.Save();
-                TempData["success"] = "The Villa has been created successfully";
+                _villaService.CreateVilla(Obj);
+                 TempData["success"] = "The Villa has been created successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -62,9 +47,8 @@ namespace Resort_Application.Controllers
 
         public IActionResult Update(int villaId)
         {
-            Villa? obj = _unitOfWork.Villa.Get(u => u.Id == villaId);
-            //Villa? obj = _db.Villas.Find(villaId);
-            //var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);
+            Villa? obj = _villaService.GetVillaById(villaId);
+         
             if (obj == null)
             {
                 return RedirectToAction("Error","Home");
@@ -78,27 +62,7 @@ namespace Resort_Application.Controllers
             
             if (ModelState.IsValid && Obj.Id>0)
             {
-
-                if (Obj.Image != null)
-                {
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(Obj.Image.FileName);
-                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
-                    if (!string.IsNullOrEmpty(Obj.ImageUrl))
-                    {
-                        var OldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, Obj.ImageUrl.TrimStart('\\'));
-                        if (System.IO.File.Exists(OldImagePath))
-                        {
-                            System.IO.File.Delete(OldImagePath);
-                        }
-                    }
-                    using (var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create))
-                        Obj.Image.CopyTo(fileStream);
-
-                    Obj.ImageUrl = @"\images\VillaImage\" + fileName;
-
-                }
-                _unitOfWork.Villa.Update(Obj);
-                _unitOfWork.Save();
+                _villaService.UpdateVilla(Obj);
                 TempData["success"] = "The Villa has been updated successfully";
                 return RedirectToAction(nameof(Index));
             }
@@ -107,7 +71,7 @@ namespace Resort_Application.Controllers
 
         public IActionResult Delete(int villaId)
         {
-            Villa? obj = _unitOfWork.Villa.Get(u => u.Id == villaId);   
+            Villa? obj = _villaService.GetVillaById(villaId);
             if (obj is null)
             {
                 return RedirectToAction("Error", "Home");
@@ -118,24 +82,18 @@ namespace Resort_Application.Controllers
         [HttpPost]
         public IActionResult Delete(Villa Obj)
         {
-            Villa? objFromDb = _unitOfWork.Villa.Get(u=> u.Id == Obj.Id);
-            if (objFromDb is not null)
-            {
+            bool deleted = _villaService.DeleteVilla(Obj.Id);
 
-                if (!string.IsNullOrEmpty(objFromDb.ImageUrl))
-                {
-                    var OldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
-                    if (System.IO.File.Exists(OldImagePath))
-                    {
-                        System.IO.File.Delete(OldImagePath);
-                    }
-                }
-                _unitOfWork.Villa.Remove(objFromDb);
-                _unitOfWork.Save();
+            if(deleted)
+            {
                 TempData["success"] = "The Villa has been deleted successfully";
                 return RedirectToAction(nameof(Index));
             }
-            TempData["error"] = "The Villa could not be deleted ";
+            else
+            {
+                TempData["error"] = "Failed to delete the villa. ";
+            }
+
             return View();
         }
 
